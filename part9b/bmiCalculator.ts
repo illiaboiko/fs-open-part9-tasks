bf077f20210760d9a0e820657d7bd7faae5cd470
illiaboiko <illiaboiko.095@gmail.com>
2025-10-28 19:54:12 +0200

    task 9.5 done

--- part9b/bmiCalculator.ts
+++ part9b/bmiCalculator.ts
@@ -1,6 +1,6 @@
 interface BMINumbers {
-  value1: number
-  value2: number
+  height: number
+  mass: number
 }
 
 const parseArguments = (args: string[]): BMINumbers => {
@@ -9,32 +9,36 @@ const parseArguments = (args: string[]): BMINumbers => {
 
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return {
-      value1: Number(args[2]),
-      value2: Number(args[3]),
+      height: Number(args[2]),
+      mass: Number(args[3]),
     }
   } else {
     throw new Error('Provided values were not numbers!')
   }
 }
 
-const BMICalculator = (a: number, b: number) => {
-  const mass = b
-  const height = a / 100
-  const BMI = mass / (height * height)
+const BMICalculator = (height: number, mass: number): string => {
+  const heightInM = height / 100
+  const BMI = mass / (heightInM * heightInM)
   console.log(BMI)
 
-  if (BMI < 25) console.log('Normal range')
-  if (BMI < 30) console.log('Overweigh range')
-  if (BMI >= 30) console.log('Obese range')
+  if (BMI < 25) return 'Normal range'
+  if (BMI < 30) return 'Overweigh range'
+  if (BMI >= 30) return 'Obese range'
+
+  return 'Unknown range'
+}
+if (require.main === module) {
+  try {
+    const { height, mass } = parseArguments(process.argv)
+    console.log(BMICalculator(height, mass))
+  } catch (error: unknown) {
+    let errorMessage = 'Smth bad happened'
+    if (error instanceof Error) {
+      errorMessage += 'Error: ' + error.message
+    }
+    console.log(errorMessage)
+  }
 }
 
-try {
-  const { value1, value2 } = parseArguments(process.argv)
-  BMICalculator(value1, value2)
-} catch (error: unknown) {
-  let errorMessage = 'Smth bad happened'
-  if (error instanceof Error) {
-    errorMessage += 'Error: ' + error.message
-  }
-  console.log(errorMessage)
-}
+export { BMICalculator, parseArguments }
--- part9b/index.ts
+++ part9b/index.ts
@@ -1,12 +1,27 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+import { BMICalculator } from './bmiCalculator'
+const app = express()
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
-});
+  res.send('Hello Full Stack!')
+})
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height)
+  const mass = Number(req.query.weight)
+
+  if (isNaN(height) || isNaN(mass)) {
+    return res.status(400).json({
+      error: 'malformatted parameters',
+    })
+  }
+
+  const BMI = BMICalculator(height, mass)
+  return res.json({ weight: mass, height: height, bmi: BMI })
+})
+
+const PORT = 3003
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
