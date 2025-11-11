7d8585d86ff2ed719e68ddfbd4b9aee2e76157a3
illiaboiko <illiaboiko.095@gmail.com>
2025-11-11 15:31:28 +0200

    taks 9.15 done

---
+++ react-tsc/src/App.tsx
@@ -0,0 +1,37 @@
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Total from "./components/Total";
+import type { CoursePart } from "./types";
+
+const App = () => {
+  const courseName: string = "Half Stack application development";
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  const totalExercises: number = courseParts.reduce(
+    (sum, part) => sum + part.exerciseCount,
+    0
+  );
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total total={totalExercises} />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-tsc/src/components/Total.tsx
@@ -0,0 +1,9 @@
+const Total = ({ total }: { total: number }) => {
+  return (
+    <div>
+      <p>Total number of exercises: {total}</p>
+    </div>
+  );
+};
+
+export default Total;
